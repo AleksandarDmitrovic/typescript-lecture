07edca022a416369c73b0fcf7a9e5e483be33488
Aleksandar <aleksandar.dmitrovic@gmail.com>
2021-02-01 20:06:34 -0700

    functions

---
+++ build/functions.js
@@ -0,0 +1,15 @@
+"use strict";
+const sayHello = (name, age) => {
+    return `hello ${name}`;
+};
+const returnValue = sayHello('Aleksandar');
+const returningNothing = (num) => {
+    console.log(num + num);
+};
+returningNothing(2);
+const returningPromise = (num) => {
+    return new Promise((res, rej) => {
+        res(num);
+    });
+};
+const returnValue2 = returningPromise(2);
---
+++ functions.ts
@@ -0,0 +1,19 @@
+const sayHello = (name: string, age?: number): boolean | string => {
+  return `hello ${name}`;
+};
+
+const returnValue = sayHello('Aleksandar');
+
+const returningNothing = (num: number): void => {
+  console.log(num + num);
+};
+
+returningNothing(2);
+
+const returningPromise = (num: number): Promise<number> => {
+  return new Promise((res, rej) => {
+    res(num);
+  });
+};
+
+const returnValue2 = returningPromise(2);

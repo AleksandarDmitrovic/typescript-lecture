be7e5d624f2aa32c6eccc410e5d5d1c5b3604f4e
Aleksandar <aleksandar.dmitrovic@gmail.com>
2021-02-01 20:25:59 -0700

    generics

---
+++ build/generics.js
@@ -0,0 +1,15 @@
+"use strict";
+// T = type convention 
+// interface IContainer<T> {
+//   title: string;
+//   content: T;
+// }
+const stringContainer = {
+    title: 'string container',
+    content: 'something',
+    somethingElse: 7
+};
+// const numberContainer: IContainer<number> = {
+//   title: 'number container',
+//   content: 42
+// };
---
+++ generics.ts
@@ -0,0 +1,22 @@
+// T = type convention 
+// interface IContainer<T> {
+//   title: string;
+//   content: T;
+// }
+
+interface IContainer<A, B> {
+  title: string;
+  content: A;
+  somethingElse: B;
+}
+
+const stringContainer: IContainer<string, number> = {
+  title: 'string container',
+  content: 'something',
+  somethingElse: 7
+};
+
+// const numberContainer: IContainer<number> = {
+//   title: 'number container',
+//   content: 42
+// };
